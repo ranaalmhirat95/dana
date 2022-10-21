83cce5b317784e99b6f9a90fbc025d88caab50bc
unknown <ranaalmhirat95>
2022-10-21 14:45:48 +0200

    upload html

---
+++ script.js
@@ -0,0 +1,19 @@
+// 01..
+let numberOne = 10;
+let numberTwo = 20;
+console.log(numberOne +""+ numberTwo);
+console.log( typeof (numberOne +""+ numberTwo));
+ console.log(`${numberOne}${numberTwo}`);
+ console.log(typeof(`${numberOne}${numberTwo}`));
+ console.log(numberTwo +"\n"+ numberOne);
+console.log(`${numberTwo}
+${numberOne}`);
+// 02..edit in html code
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
+// 03 ..
+console.log("\`I\'m In \n \\\\ \n Love \\\\ \"\"\" \'\'\'\n \+\+ With \+\+ \n \\\"\"\"\\\"\"\"\n \"\"JavaScript\"\"\`\`");
+// 04 ..
+let a = 21;
+let b = 20;
+console.log(`_${a}_${b}${a}_${b}${a}_${b}`);
